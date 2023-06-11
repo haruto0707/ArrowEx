c6ea253001a7cb23257a6cc410a5475b41483462
haruto0707 <221205070@ccmailg.meijo-u.ac.jp>
2023-06-11 15:21:52 +0900

    First commit

---
+++ src/main/java/org/hark7/arrowex/ArrowEx.java
@@ -0,0 +1,25 @@
+package org.hark7.arrowex;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Arrow;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ArrowEx extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onArrowHit(ProjectileHitEvent event) {
+        if (event.getEntity() instanceof Arrow) {
+            Arrow arrow = (Arrow) event.getEntity();
+            arrow.getWorld().createExplosion(arrow.getLocation(), 30.0F, false, true);
+            arrow.remove();
+        }
+    }
+}
